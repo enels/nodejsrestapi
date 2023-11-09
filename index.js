7327367d1c61c5d8beae2e016d445a7ace2640b1
Enoma Uwaifo <enomauwaifo@gmail.com>
2023-11-09 12:11:13 +0100

    parsing request path

--- index.js
+++ index.js
@@ -5,10 +5,24 @@
 
 // Dependencie
 var http = require('http');
+var url = require('url');
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res){
-    res.end("Hello, World\n");
+
+    // get the url and parse it
+    var parseUrl = url.parse(req.url, true);
+
+    // get the path
+    var path = parseUrl.pathname;
+    var trimmedPath = path.replace(/\/+|\/+$/g, '');
+
+    // send the response
+    res.end("Hello, World!");
+
+    // log the request path
+    console.log("Request received as path " + trimmedPath);
+
 });
 // Start the server and have it listen on port 3000
 server.listen(3000, function() {
